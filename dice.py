4f2aaba1c3975de1d433113fb748a8a6d4f75a6a
Atrociou <56047111+Atrociou@users.noreply.github.com>
2019-10-04 13:02:33 -0700

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,25 @@
+# Name: Andrew Contreras
+# Period 4
+# Dice Rolling Simulator
+import random
+
+rolls = int(input("how many rolls"))
+
+a1 = 0
+b1 = 0
+c1 = 0
+d1 = 0
+e1 = 0
+f1 = 0
+x = 1
+
+while x <= rolls:
+	randomNum = random.randint(1, 6)
+	if randomNum == 
+	
+
+	   
+		
+
+	
+	
